e8022b4ffc005b1e3082813333f5f3e1ee423cfb
amyanycoder <amyfalk@protonmail.com>
2023-04-11 10:32:08 -0400

    Multiple tables in database

--- monkeysmash/src/dictionary.rs
+++ monkeysmash/src/dictionary.rs
@@ -10,14 +10,23 @@ pub fn create_dictionary() {
 
         CREATE TABLE adverbs (id INTEGER PRIMARY KEY, word TEXT);
 
-        CREATE TABLE prepositions (id INTEGER PRIMARY KEY, word TEXT;
+        CREATE TABLE prepositions (id INTEGER PRIMARY KEY, word TEXT);
 
-        CREATE TABLE transitive verbs (id INTEGER PRIMARY KEY, word TEXT);
+        CREATE TABLE transitive_verbs (id INTEGER PRIMARY KEY, word TEXT);
 
-        CREATE TABLE plural nouns (id INTEGER PRIMARY KEY, word TEXT);
+        CREATE TABLE plural_nouns (id INTEGER PRIMARY KEY, word TEXT);
 
-        CREATE TABLE past participle (id INTEGER PRIMARY KEY, word TEXT);
+        CREATE TABLE past_participle (id INTEGER PRIMARY KEY, word TEXT);
         
+        CREATE TABLE present_participle (id INTEGER PRIMARY KEY, word TEXT);
+
+        CREATE TABLE indicative_verbs (id INTEGER PRIMARY KEY, word TEXT);
+
+        CREATE TABLE interjections (id INTEGER PRIMARY KEY, word TEXT);
+
+        CREATE TABLE conjunctions (id INTEGER PRIMARY KEY, word TEXT);
+
+        CREATE TABLE pronouns (id INTEGER PRIMARY KEY, word TEXT);
 
     ";
 
@@ -25,12 +34,133 @@ pub fn create_dictionary() {
 
 }
 
-pub fn fill_dictionary(word: Word){
+pub fn fill_nouns(word: Word){
     let connection = sqlite::open("dictionary.db").unwrap();
 
     let query = format!("
-        INSERT INTO words (word, pos) VALUES ('{}', '{}');
-    ", word.0, word.1);
+        INSERT INTO nouns (word) VALUES ('{}');
+    ", word.0);
+
+    println!("{}", query);
+    connection.execute(query).unwrap();
+}
+
+pub fn fill_adjectives(word: Word){
+    let connection = sqlite::open("dictionary.db").unwrap();
+
+    let query = format!("
+        INSERT INTO adjectives (word) VALUES ('{}');
+    ", word.0);
+
+    println!("{}", query);
+    connection.execute(query).unwrap();
+}
+
+pub fn fill_adverbs(word: Word){
+    let connection = sqlite::open("dictionary.db").unwrap();
+
+    let query = format!("
+        INSERT INTO adverbs (word) VALUES ('{}');
+    ", word.0);
+
+    println!("{}", query);
+    connection.execute(query).unwrap();
+}
+
+pub fn fill_prepositions(word: Word){
+    let connection = sqlite::open("dictionary.db").unwrap();
+
+    let query = format!("
+        INSERT INTO prepositions (word) VALUES ('{}');
+    ", word.0);
+
+    println!("{}", query);
+    connection.execute(query).unwrap();
+}
+
+pub fn fill_trans(word: Word){
+    let connection = sqlite::open("dictionary.db").unwrap();
+
+    let query = format!("
+        INSERT INTO transitive_verbs (word) VALUES ('{}');
+    ", word.0);
+
+    println!("{}", query);
+    connection.execute(query).unwrap();
+}
+
+pub fn fill_plural(word: Word){
+    let connection = sqlite::open("dictionary.db").unwrap();
+
+    let query = format!("
+        INSERT INTO plural_nouns (word) VALUES ('{}');
+    ", word.0);
+
+    println!("{}", query);
+    connection.execute(query).unwrap();
+}
+
+pub fn fill_past_participle(word: Word){
+    let connection = sqlite::open("dictionary.db").unwrap();
+
+    let query = format!("
+        INSERT INTO past_participle (word) VALUES ('{}');
+    ", word.0);
+
+    println!("{}", query);
+    connection.execute(query).unwrap();
+}
+
+pub fn fill_present_participle(word: Word){
+    let connection = sqlite::open("dictionary.db").unwrap();
+
+    let query = format!("
+        INSERT INTO present_participle (word) VALUES ('{}');
+    ", word.0);
+
+    println!("{}", query);
+    connection.execute(query).unwrap();
+}
+
+pub fn fill_indicative(word: Word){
+    let connection = sqlite::open("dictionary.db").unwrap();
+
+    let query = format!("
+        INSERT INTO indicative_verbs (word) VALUES ('{}');
+    ", word.0);
+
+    println!("{}", query);
+    connection.execute(query).unwrap();
+}
+
+pub fn fill_interjections(word: Word){
+    let connection = sqlite::open("dictionary.db").unwrap();
+
+    let query = format!("
+        INSERT INTO interjections (word) VALUES ('{}');
+    ", word.0);
+
+    println!("{}", query);
+    connection.execute(query).unwrap();
+}
+
+pub fn fill_conjunctions(word: Word){
+    let connection = sqlite::open("dictionary.db").unwrap();
+
+    let query = format!("
+        INSERT INTO conjunctions (word) VALUES ('{}');
+    ", word.0);
+
+    println!("{}", query);
+    connection.execute(query).unwrap();
+}
+
+pub fn fill_pronouns(word: Word){
+    let connection = sqlite::open("dictionary.db").unwrap();
+
+    let query = format!("
+        INSERT INTO pronouns (word) VALUES ('{}');
+    ", word.0);
 
     println!("{}", query);
     connection.execute(query).unwrap();
@@ -40,7 +170,18 @@ pub fn clear_dictionary() {
     let connection = sqlite::open("dictionary.db").unwrap();
 
     let query = "
-        DROP TABLE words;
+        DROP TABLE nouns;
+        DROP TABLE adjectives;
+        DROP TABLE adverbs;
+        DROP TABLE conjunctions;
+        DROP TABLE indicative_verbs;
+        DROP TABLE interjections;
+        DROP TABLE past_participle;
+        DROP TABLE plural_nouns;
+        DROP TABLE prepositions;
+        DROP TABLE present_participle;
+        DROP TABLE pronouns;
+        DROP TABLE transitive_verbs;
     ";
 
     connection.execute(query).unwrap(); 
--- monkeysmash/src/parser.rs
+++ monkeysmash/src/parser.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::fmt;
-
-use crate::dictionary::fill_dictionary;
+use std::string::String;
+use crate::dictionary::*;
 
 
 //Word(word, part of speech)
@@ -24,13 +24,56 @@ pub fn parse_file() -> Result<(), Box<dyn Error>>{
 
         let current_word = Word(String::from(value.get(0).unwrap()), String::from(value.get(1).unwrap()));
 
-        //println!("{:}", current_word);
-
-        fill_dictionary(current_word);
-        
-
+        handle_filing(current_word);
     }
 
     Ok(())
 
+}
+
+fn handle_filing(current_word: Word){
+    println!("{:}", current_word);
+
+    //
+
+    //logic tree determining word table based on part of speech
+    //eliminates words with more than 10 characters
+    if current_word.1.chars().count() > 10{
+
+    }
+    else if current_word.1.contains("pl."){
+        fill_plural(current_word);
+    }
+    else if current_word.1.contains("v. i."){
+        fill_indicative(current_word);
+    }
+    else if current_word.1.contains("n."){
+        fill_nouns(current_word);
+    }
+    else if current_word.1.contains("a."){
+        fill_adjectives(current_word);
+    }
+    else if current_word.1.contains("adv."){
+        fill_adverbs(current_word);
+    }
+    else if current_word.1.contains("v. t."){
+        fill_trans(current_word);
+    }
+    else if current_word.1.contains("p. p."){
+        fill_past_participle(current_word);
+    }
+    else if current_word.1.contains("pr. p."){
+        fill_present_participle(current_word);
+    }
+    else if current_word.1.contains("prep."){
+        fill_prepositions(current_word);
+    }
+    else if current_word.1.contains("interj."){
+        fill_interjections(current_word);
+    }
+    else if current_word.1.contains("conj."){
+        fill_conjunctions(current_word);
+    }
+
+    
 }
